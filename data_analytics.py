83d3d21d2e1d005940925f4ecc52da84869d3f2f
Walter <walterdirvin@gmail.com>
2023-07-29 14:03:27 -0400

    refactored data analysis to use class structure

--- data_analytics.py
+++ data_analytics.py
@@ -1,97 +1,42 @@
-import savings
+class DataAnalysis:
+    def __init__(self, starting, salary, salary_bonus, salary_cap, interest):
+        self.starting = starting
+        self.salary = salary
+        self.salary_bonus = salary_bonus
+        self.salary_cap = salary_cap
+        self.interest = interest
 
+    def salary_growth(self, years):
+        salary = self.salary
+        for i in range(years):
+            if salary + self.salary_bonus < self.salary_cap:
+                salary += self.salary_bonus
+            else:
+                return self.salary_cap
+        return salary
 
-def get_total_interest(starting, amount, bonus, rate, years):
-    """Gets the total gained from interest by subtracting the amount gained from simply saving at 0% gain
+    def growth(self, years, bonus=False, interest=False):
+        base = self.starting
+        total = self.starting
+        salary = self.salary
+        for i in range(years):
+            total += salary
+            base += salary
+            if bonus:
+                salary = self.salary_growth(i)
+            if interest:
+                total += total * self.interest
+        return [total, base]
 
-    :param starting: - starting amount
-    :param amount: - amount that is added per year
-    :param bonus: - amount to add on to amount per year
-    :param rate: - yearly interest rate
-    :param years: - how many years to calculate for
-    :return total: - the total amount of purely interest grown (compared to just saving)
-    """
-    base_amount = savings.complex_saving_rate(starting, amount, bonus, years)
-    interest_amount = savings.complex_compound_saving_rate(starting, amount, bonus, years, rate)
-    total = interest_amount - base_amount
-    return total
-
-
-def salary_growth_calculator(starting, bonus, years, cap):
-    """More realistic salary growth curve, where there is ultimately a limit to how much can be made, meaning
-    bonus will not apply once the cap is reached.
-
-    :param starting: - the starting point for salary
-    :param bonus: - expected growth in salary per year of experience
-    :param years: - years working
-    :param cap: - the salary ceiling for the particular field
-    :return amount: - the salary expected after the number of years input
-    """
-    total = starting
-    for i in range(years):
-        if total + bonus < cap:
-            total += bonus
-        else:
-            return cap
-    return total
-
-
-def salary_simple_savings(starting, amount, bonus, cap, years):
-    """Returns the total amount gained by salary with no interest rate.
-
-    :param starting:
-    :param amount:
-    :param bonus:
-    :param cap:
-    :param years:
-    :return:
-    """
-    total = starting
-    cur_salary = amount
-    for i in range(years):
-        total += cur_salary
-        cur_salary = salary_growth_calculator(cur_salary, bonus, years, cap)
-    return total
-
-
-def salary_interest_savings(starting, amount, bonus, cap, rate, years):
-    """Gets total amount of savings generated with fixed interest rate, implemented with a cap on total salary.
-
-    :param starting: - starting point for savings
-    :param amount: - amount of salary currently gained
-    :param bonus: - amount expected to rise per year on salary
-    :param cap: - total amount salary can raise to
-    :param rate: - interest rate per year
-    :param years: - years to calculate for
-    :return total: - total amount saved with capped salary and interest
-    """
-    total = starting
-    cur_salary = amount
-    for i in range(years):
-        total += cur_salary
-        total += total * rate
-        cur_salary = salary_growth_calculator(cur_salary, bonus, 1, cap)
-    return total
-
-
-def salary_interest_savings_total_interest(starting, amount, bonus, cap, rate, years):
-    """Calculates total interest generated over specified years given a salary with a cap, and fixed interest rate.
-
-    :param starting: - starting point for savings
-    :param amount: - amount of salary currently gained
-    :param bonus: - amount expected to rise per year on salary
-    :param cap: - total amount salary can raise to
-    :param rate: - interest rate per year
-    :param years: - years to calculate for
-    :return total: - total amount of interest generated
-    """
-    interest_amount = starting
-    base_amount = starting
-    cur_salary = amount
-    for i in range(years):
-        base_amount += savings.simple_saving_rate(base_amount, cur_salary, 1)
-        interest_amount += savings.simple_compound_saving_rate(interest_amount, cur_salary, 1, rate)
-        cur_salary = salary_growth_calculator(cur_salary, bonus, 1, cap)
-
-    total = interest_amount - base_amount
-    return total
+    def print_report(self, years):
+        total, base = self.growth(years, True, True)
+        interest = total - base
+        print("Generating report for the following conditions:"
+              f"Starting amount: {self.starting}\n"
+              f"Starting salary: {self.salary}\n"
+              f"Salary increase per year: {self.salary_bonus}\n"
+              f"Salary Cap: {self.salary_cap}\n"
+              f"Interest per Year: {self.interest}\n"
+              f"Total Interest generated: {interest}\n"
+              f"Total base saved: {base}\n"
+              f"Total saved: {total}\n")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import savings
 import data_analytics
 
 
@@ -12,13 +11,11 @@ def interactive_savings_calculator():
     """
     saving_result = 0
     while 1:
-        command = input("(Q)uit, (R)eset calculations, (C)ontinue calculation further: ")
+        command = input("(Q)uit, (C)ontinue calculation further: ")
         if command == "q" or command == "Q":
             break
-        elif command == "r" or command == "R":
-            saving_result = setup_comparison()
         elif command == "c" or command == "C":
-            saving_result = setup_comparison(saving_result)
+            setup_comparison(saving_result)
         else:
             print("Invalid command!")
 
@@ -28,25 +25,17 @@ def setup_comparison(starting=0):
         print(f"Starting from {starting}, enter the following details to get your report.")
         amount = int(input("Amount to save per year: "))
         bonus = int(input("Amount you can expect to get as bonus per year on top of what you make: "))
+        cap = int(input("Amount your field tends to cap out on salary: "))
         rate = float(input("Interest rate per year: "))
         years = int(input("Number of years to calculate out for: "))
-        print(f"Starting: {starting}, Amount: {amount}, Bonus: {bonus}, Rate: {rate}, Years: {years}")
+        print(f"Starting: {starting}, Amount: {amount}, Bonus: {bonus}, Cap: {cap}, Rate: {rate}, Years: {years}")
         confirmation = input("Is this correct (Y/n): ")
         if confirmation == "n" or confirmation == "N":
             print("input discarded, retrying...\n")
         else:
             break
-    simple_saving = savings.simple_saving_rate(starting, amount, years)
-    complex_saving = data_analytics.salary_simple_savings(starting, amount, bonus, 100000, years)
-    simple_interest = savings.simple_compound_saving_rate(starting, amount, years, rate)
-    complex_interest = savings.complex_compound_saving_rate(starting, amount, bonus, years, rate)
-    interest_amt = data_analytics.get_total_interest(starting, amount, bonus, rate, years)
-    print("The results are as follows: ")
-    print(f"Simple savings: {simple_saving}, Complex savings: {complex_saving}, "
-          f"Simple interest: {simple_interest}, Complex interest: {complex_interest}, "
-          f"Total interest gained: {interest_amt}")
-
-    return complex_interest
+    data_analyser = data_analytics.DataAnalysis(starting, amount, bonus, cap, rate)
+    data_analyser.print_report(years)
 
 
 if __name__ == '__main__':
