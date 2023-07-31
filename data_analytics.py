dec70df5954bf710ffccb05e01ef8be3ebc880df
Walter <walterdirvin@gmail.com>
2023-07-31 19:51:23 -0400

    initial rework of sidebar

--- data_analytics.py
+++ data_analytics.py
@@ -1,42 +1,48 @@
-class DataAnalysis:
-    def __init__(self, starting, salary, salary_bonus, salary_cap, interest):
-        self.starting = starting
-        self.salary = salary
-        self.salary_bonus = salary_bonus
-        self.salary_cap = salary_cap
-        self.interest = interest
+import panel as pn
 
+class DataAnalysis:
+    def __init__(self):
+        self.starting = pn.widgets.FloatInput(name="Starting funds")
+        self.salary = pn.widgets.FloatInput(name="Income")
+        self.salary_bonus = pn.widgets.FloatInput(name="Bonus per year")
+        self.salary_cap = pn.widgets.FloatInput(name="Income Ceiling")
+        self.interest_rate = pn.widgets.FloatInput(name="Interest rate")
+        self.years = pn.widgets.IntSlider(name="Years", start=0, end=100, value=0)
+
+    def return_sidebar(self):
+        return [self.starting, self.salary, self.salary_bonus, self.salary_cap, self.interest_rate, self.years]
+    
     def salary_growth(self, years):
-        salary = self.salary
+        salary = self.salary.value
         for i in range(years):
-            if salary + self.salary_bonus < self.salary_cap:
-                salary += self.salary_bonus
+            if salary + self.salary_bonus.value < self.salary_cap.value:
+                salary += self.salary_bonus.value
             else:
-                return self.salary_cap
+                return self.salary_cap.value
         return salary
 
     def growth(self, years, bonus=False, interest=False):
-        base = self.starting
-        total = self.starting
-        salary = self.salary
+        base = self.starting.value
+        total = self.starting.value
+        salary = self.salary.value
         for i in range(years):
             total += salary
             base += salary
             if bonus:
                 salary = self.salary_growth(i)
             if interest:
-                total += total * self.interest
+                total += total * self.interest_rate.value
         return [total, base]
 
     def print_report(self, years):
         total, base = self.growth(years, True, True)
         interest = total - base
         print("Generating report for the following conditions:"
-              f"Starting amount: {self.starting}\n"
-              f"Starting salary: {self.salary}\n"
-              f"Salary increase per year: {self.salary_bonus}\n"
-              f"Salary Cap: {self.salary_cap}\n"
-              f"Interest per Year: {self.interest}\n"
-              f"Total Interest generated: {interest}\n"
-              f"Total base saved: {base}\n"
-              f"Total saved: {total}\n")
+              f"Starting amount: {self.starting.value}\n"
+              f"Starting salary: {self.salary.value}\n"
+              f"Salary increase per year: {self.salary_bonus.value}\n"
+              f"Salary Cap: {self.salary_cap.value}\n"
+              f"Interest per Year: {self.interest_rate.value}\n"
+              f"Total Interest generated: {interest.value}\n"
+              f"Total base saved: {base.value}\n"
+              f"Total saved: {total.value}\n")
