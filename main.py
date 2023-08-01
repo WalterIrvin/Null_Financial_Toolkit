c9ab0171c006d9c3a1f90e4fa370a52a52219587
Walter <walterdirvin@gmail.com>
2023-07-31 21:36:08 -0400

    initial graphs linked

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import panel as pn
 if __name__ == '__main__':
     data_analyser = data_analytics.DataAnalysis()
     tabs = pn.Tabs(
-        ("Main", pn.Column("Example text here.")),
-        ("Salary Info", pn.Column("WIP")),
-        ("Interest Table", pn.Column("WIP")),
+        ("Main", pn.Column(data_analyser.view_main())),
+        ("Salary Info", pn.Column(data_analyser.view_salary_info())),
+        ("Interest Table", pn.Column(data_analyser.view_interest_info())),
         dynamic=True
     )
     template = pn.template.FastListTemplate(title="Financial Toolkit", sidebar=data_analyser.return_sidebar(),
